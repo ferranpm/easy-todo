f8ae7bdfdf5c945de5c47223d3424bed0812da82
Ferran Pelayo Monfort <ferran.pel.mon@gmail.com>
2013-10-12 14:30:41 +0200

    Changed connect_db() to a class and using "with"

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ from flask import url_for
 app = Flask(__name__)
 DATABASE = '/home/ferran/WebApps/ToDo/database'
 
-def connect_db():
-	return sqlite3.connect(DATABASE)
+class Connection:
+	def __enter__(self):
+		self.conn = sqlite3.connect(DATABASE)
+		return self.conn.cursor()
+
+	def __exit__(self, type, value, traceback):
+		self.conn.commit()
+		self.conn.close()
 
 @app.route('/')
 def index():
@@ -17,39 +23,27 @@ def index():
 
 @app.route('/todo/<list_id>')
 def todo(list_id):
-	conn = connect_db()
-	c = conn.cursor()
-	c.execute('SELECT todo, done, id FROM item WHERE list_id=(?)', (list_id,))
-	todo = c.fetchall()
-	conn.close()
+	with Connection() as c:
+		c.execute('SELECT todo, done, id FROM item WHERE list_id=(?)', (list_id,))
+		todo = c.fetchall()
 	return render_template('todo.html', todo=todo, list_id=list_id)
 
 @app.route('/add/<list_id>', methods=['POST'])
 def add_item(list_id):
 	text = request.form['todo']
-	conn = connect_db()
-	c = conn.cursor()
-	c.execute('INSERT INTO item (list_id, todo, done) VALUES (?, ?, 0)', (list_id, text,))
-	conn.commit()
-	conn.close()
-	url = '/todo/%s' % list_id
+	with Connection() as c:
+		c.execute('INSERT INTO item (list_id, todo, done) VALUES (?, ?, 0)', (list_id, text,))
 	return redirect(url_for('todo', list_id=list_id))
 
 @app.route('/mark/<item_id>', methods=['POST'])
 def mark(item_id):
-	conn = connect_db()
-	c = conn.cursor()
-	c.execute('UPDATE item SET done=1 WHERE id=?', item_id)
-	conn.commit()
-	conn.close()
+	with Connection() as c:
+		c.execute('UPDATE item SET done=1 WHERE id=?', item_id)
 
 @app.route('/unmark/<item_id>', methods=['POST'])
 def unmark(item_id):
-	conn = connect_db()
-	c = conn.cursor()
-	c.execute('UPDATE item SET done=0 WHERE id=?', item_id)
-	conn.commit()
-	conn.close()
+	with Connection() as c:
+		c.execute('UPDATE item SET done=0 WHERE id=?', item_id)
 
 if __name__ == '__main__':
 	app.debug = True
