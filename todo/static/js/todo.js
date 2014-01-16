344da43e8292db12b7ff62d8e4449220d2e4b84a
Ferran Pelayo Monfort <ferran.pel.mon@gmail.com>
2014-01-16 11:51:42 +0100

    Separated js file for scripts

---
+++ todo/static/js/todo.js
@@ -0,0 +1,20 @@
+function toggle(e) {
+    e.stopPropagation();
+    var tr = $(e.target).closest('tr');
+    var ch = tr.find('input[type="checkbox"]');
+    var id = tr.data('id');
+    if (tr.hasClass('done')) {
+        jQuery.post('/unmark/' + id);
+        tr.removeClass('done');
+        ch.prop('checked', false);
+    }
+    else {
+        jQuery.post('/mark/' + id);
+        tr.addClass('done');
+        ch.prop('checked', true);
+    }
+}
+
+function a_handler(e) {
+    e.stopPropagation();
+}
