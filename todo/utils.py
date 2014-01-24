ab3155014ed0df6cb17ad2e9db48682af9b18eb7
Ferran Pelayo Monfort <ferran.pel.mon@gmail.com>
2014-01-24 10:31:17 +0100

    Always check if logged in

--- todo/utils.py
+++ todo/utils.py
@@ -4,6 +4,21 @@ from contextlib import closing
 from . import DATABASE
 from . import app
 
+class Connection:
+
+    def __init__(self, path):
+        self.path = path
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.path)
+        return self.conn.cursor()
+
+    def __exit__(self, type, value, traceback):
+        self.conn.commit()
+        self.conn.close()
+
+connection = Connection(DATABASE)
+
 def db_connect():
     return sqlite3.connect(DATABASE)
 
@@ -18,7 +33,6 @@ def db_exists():
 
 def db_valid_todo_id(list_id):
     valid_length = len(list_id) > 0
-    connection = Connection(DATABASE)
     exists = True
     with connection as c:
         c.execute('SELECT * FROM todos WHERE list_id=?', (list_id,))
@@ -26,18 +40,21 @@ def db_valid_todo_id(list_id):
             exists = False
     return not exists and valid_length
 
+def has_password(list_id):
+    with connection as c:
+        c.execute('SELECT password FROM todos WHERE list_id=?', (list_id,))
+        password = c.fetchone()[0]
+    return len(password) > 0
+
 def has_permission(list_id, cookies):
-    return True
+    password = has_password(list_id)
+    hashed_password = ''
+    if password:
+        with connection as c:
+            c.execute('SELECT password FROM todos WHERE list_id=?', (list_id,))
+            hashed_password = c.fetchone()[0]
+    cookie = False
+    if 'password' in cookies:
+        cookie = cookies['password'] == hashed_password
+    return (password and cookie) or (not password)
 
-class Connection:
-
-    def __init__(self, path):
-        self.path = path
-
-    def __enter__(self):
-        self.conn = sqlite3.connect(self.path)
-        return self.conn.cursor()
-
-    def __exit__(self, type, value, traceback):
-        self.conn.commit()
-        self.conn.close()
